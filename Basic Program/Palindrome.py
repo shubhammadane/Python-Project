a4ac03f4fca4452c418ba82b7107d88c2fda9c49
Shubham Madane <shubhammadane@gmail.com>
2026-01-21 01:26:54 +0530

    change location

---
+++ Basic Program/Palindrome.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+temp = num
+rev = 0 
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num = num // 10 
+if temp == rev:
+    print(f"{temp} is a Palindrome number")
+else:
+    print(f"{temp} is not a Palindrome number")
