fd052dcfcc2578be5db80ed96ecc5c4616a8b4ce
Shubham Madane <shubhammadane@gmail.com>
2026-01-23 13:34:42 +0530

    dict and list

---
+++ ListUsed in loop/simple1.py
@@ -0,0 +1,8 @@
+list = ["Apple", "Banana", "Mango", "Grapes", "Orange" ]
+for fruit in list:
+    print(fruit)
+
+print("Printing with index")
+for index in range(len(list)):
+    print(list[index])
+    
---
+++ dict in loop/InfoStudents.py
@@ -0,0 +1,8 @@
+student = {
+     "Name": "Shubham Madane",
+     "Collage": "GecaCh",
+     "Branch": "Computer",
+     "RollNo": "MT24F05F007"
+}
+for key, value in student.items():
+    print(key,":", value) 
