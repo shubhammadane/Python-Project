fe07d0fbdbfed0c7aed5ede614efa03db0254874
Shubham Madane <shubhammadane@gmail.com>
2026-01-23 12:47:54 +0530

    Square Program

---
+++ loop Program/For Loop/Simple1.py
@@ -0,0 +1,4 @@
+i=1
+for val in range(5):
+    print(i)
+    i=i+1
---
+++ loop Program/Pattern Program/Square.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    for j in range(5):
+        print("* ", end="")
+    print()
+
+     #Memory Trick  
+    #Rows = outer loop
+    #Columns = inner loop
+    
+
+# end="":
+# Prevents moving to next line after printing *
+# print():
+# Moves cursor to next line after one row is printed
